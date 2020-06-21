f41c81c659b5d1bca537aa367070c83500588a46
wangze <73400@163.com>
2020-06-21 18:15:55 +0800

    :bug: 留言页拒绝授权登录的提示信息

--- pages/about/guestbook/index.js
+++ pages/about/guestbook/index.js
@@ -78,11 +78,15 @@ Page({
    * @param {*} e 
    */
   getUser(e){
-    wx.setStorageSync(Config.User, e.detail.userInfo);
-    this.setData({
-      modalName: null,
-    })
-    apiResult.success("登录成功");
+    if (!e.detail.userInfo) {
+      return apiResult.error("登录失败");
+    } else {
+      wx.setStorageSync(Config.User, e.detail.userInfo);
+      this.setData({
+        modalName: null,
+      })
+      return apiResult.success("登录成功");
+    }
   },
   /**
    * 评论者输入邮箱
