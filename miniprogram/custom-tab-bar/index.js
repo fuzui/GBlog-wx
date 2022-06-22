039f18e981fd96cc7de0175d50e99a0b2291d5fb
fuzui <73400@163.com>
2022-06-23 01:23:16 +0800

    feat: 标签栏自定义

--- miniprogram/custom-tab-bar/index.js
+++ miniprogram/custom-tab-bar/index.js
@@ -1,4 +1,5 @@
-// 获取应用实例
+import { THEME_SETTING_KEY } from '../services/const-data/theme-setting-key'
+
 const app = getApp()
 Component({
   data: {
@@ -41,7 +42,22 @@ Component({
     // 适配IphoneX的屏幕底部横线
     isIphoneX: app.globalData.isIphoneX
   },
-  attached() {},
+  lifetimes: {
+    async attached() {
+      if (!app.globalData.hasInit) {
+        await app.init()
+      }
+      let list = this.data.list
+      list[0].text = app.themeSettings[THEME_SETTING_KEY.TAB_BAR_HOME] || list[0].text
+      list[1].text = app.themeSettings[THEME_SETTING_KEY.TAB_BAR_CATEGORY] || list[1].text
+      list[2].text = app.themeSettings[THEME_SETTING_KEY.TAB_BAR_PHOTO] || list[2].text
+      list[3].text = app.themeSettings[THEME_SETTING_KEY.TAB_BAR_ABOUT] || list[3].text
+      list[4].text = app.themeSettings[THEME_SETTING_KEY.TAB_BAR_ADMIN] || list[4].text
+      this.setData({
+        list: list
+      })
+    }
+  },
   methods: {
     switchTab(e) {
       const that = this
